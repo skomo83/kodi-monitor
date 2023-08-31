b6e0ef07e533c1aa9ee9af07952c7bd0e9d823d5
skomo83 <ashley.schofield83@gmail.com>
2023-08-31 15:16:16 +1000

    Update kodistart.sh

--- kodistart.sh
+++ kodistart.sh
@@ -2,7 +2,7 @@
 
 # Restart the target service and start kodi
 # add this to sudoers file
-# username ALL=(ALL) NOPASSWD: /bin/systemctl restart kodi-monitor.service
+# username ALL=(ALL) NOPASSWD: /bin/systemctl restart kodi-remote.service
 
 sudo systemctl restart kodi-remote.service && kodi
 
